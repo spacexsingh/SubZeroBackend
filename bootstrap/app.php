816f5a92c7552f08724dcbc27f0d39cc16ad4006
singh <singh@singh.com>
2025-11-12 21:57:37 -0500

    Fix HTTPS mixed content issue

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'check.user.type' => \App\Http\Middleware\CheckUserType::class,
         ]);
